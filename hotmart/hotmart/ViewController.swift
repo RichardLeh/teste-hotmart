d8531bba75d4635771f6ef971e124a0ec5382b20
Richard Leh <richard@guarulhos.org>
2017-02-03 21:06:44 -0200

    start

---
+++ hotmart/hotmart/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hotmart
+//
+//  Created by Richard Leh on 03/02/2017.
+//  Copyright © 2017 Richard Leh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
